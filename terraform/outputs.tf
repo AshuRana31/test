f14567ebeb1481e1898d166da36fd606f623b345
AshuRana31 <41803021+AshuRana31@users.noreply.github.com>
2025-07-07 18:02:21 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,1 @@
-output "bucket_name" {
-  description = "Name of the created S3 bucket"
-  value       = aws_s3_bucket.example.bucket
-}
 
-output "bucket_arn" {
-  description = "ARN of the created S3 bucket"
-  value       = aws_s3_bucket.example.arn
-}
