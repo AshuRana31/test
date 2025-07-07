d244c4217d7af299b1f8c2851d38d93f71e57736
AshuRana31 <41803021+AshuRana31@users.noreply.github.com>
2025-07-07 18:06:39 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,22 +11,20 @@ terraform {
       version = "~> 3.1"
     }
   }
-  
+
   backend "s3" {
     bucket         = "terraform-state-ashurana31-test-1751878282"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks-ashurana31-test"
     encrypt        = true
+    role_arn       = "arn:aws:iam::038751964618:role/GitHubActionsRole"
   }
+
 }
 
 provider "aws" {
   region = var.aws_region
-
-  assume_role {
-    role_arn = "arn:aws:iam::038751964618:role/GitHubActionsRole"
-  }
   
   default_tags {
     tags = {
