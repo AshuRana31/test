b035efed2cf88604e93669435b2fa31ae3183d4c
AshuRana31 <41803021+AshuRana31@users.noreply.github.com>
2025-07-07 18:30:46 +0530

    feat Add S3 bucket test-backstage-ashurana31 configuration

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,46 @@
-variable "aws_region" {
-  description = "AWS region"
+variable "account_id" {
+  description = "AWS Account ID where the S3 bucket will be created"
   type        = string
-  default     = "us-east-1"
 }
 
-variable "aws_account_id" {
-  description = "AWS Account ID"
+variable "region" {
+  description = "AWS region where the S3 bucket will be created"
   type        = string
-  default     = "038751964618"
+}
+
+variable "environment" {
+  description = "Environment (e.g., development, staging, production)"
+  type        = string
+}
+
+variable "bucket_name" {
+  description = "Name of the S3 bucket"
+  type        = string
+}
+
+variable "versioning_enabled" {
+  description = "Whether to enable versioning for the S3 bucket"
+  type        = bool
+  default     = false
+}
+
+variable "environment_tag" {
+  description = "Environment tag for the S3 bucket"
+  type        = string
+}
+
+variable "project_tag" {
+  description = "Project name tag"
+  type        = string
+}
+
+variable "owner_tag" {
+  description = "Owner/Team responsible for this resource"
+  type        = string
+}
+
+variable "additional_tags" {
+  description = "Additional tags in JSON format"
+  type        = string
+  default     = "{}"
 }
